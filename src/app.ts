452f8d27df05c6a6efe6b96570d2e28437bf2ea8
Itokiana <itokianarakt@gmail.com>
2026-08-23 12:38:51 +0300

    fix(app): mount exam routes at /api/exams

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import examRoutes from "./routes/examRoutes";
 import { notFoundMiddleware } from "./middlewares/notFoundMiddleware";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 
@@ -7,6 +8,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/exams", examRoutes);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
