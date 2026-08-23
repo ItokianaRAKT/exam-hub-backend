f9a1b8e5fa6b9e904a4a6af80c5787defe94e76e
FRANÇOISE Anthonia <hei.anthonia@gmail.com>
2026-08-23 13:49:01 +0300

    feat: add questions , choice, exam models

--- src/models/choiceModel.ts
+++ src/models/choiceModel.ts
@@ -1,1 +1,12 @@
-// TO DO
+export interface Choice {
+    id: string;
+    questionId: string;
+    label: string;
+    isCorrect: boolean;
+}
+
+export interface CreateChoice {
+    label: string;
+    isCorrect: boolean;
+}
+
--- src/models/questionModel.ts
+++ src/models/questionModel.ts
@@ -1,1 +1,22 @@
-// TO DO
+import {CreateChoice} from "./choiceModel";
+
+export interface Question {
+    id: string;
+    examId: string;
+    text: string;
+    points: number;
+}
+
+export interface CreateQuestion {
+    statement: string;
+    points: number;
+    choices: CreateChoice[];
+}
+
+export interface UpdateQuestion {
+    statement: string;
+    points: number;
+    choices: CreateChoice[];
+}
+
+
