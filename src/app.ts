28b97141cbd8cbf86c7a2d5838970bc01e0f5408
Itokiana <itokianarakt@gmail.com>
2026-08-23 11:09:43 +0300

    feat(app): wire error middlewares into Express app
    - Mount notFoundMiddleware after all routes
    - Mount errorMiddleware as last handler in the chain
    - Add express.json() for JSON body parsing

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-// TO DO
+import express from "express";
+import { notFoundMiddleware } from "./middlewares/notFoundMiddleware";
+import { errorMiddleware } from "./middlewares/errorMiddleware";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+export default app;
