10ca85b82c07bca3fe0e1d9b41a544f9761f2e9b
Itokiana <itokianarakt@gmail.com>
2026-08-24 09:13:50 +0300

    feat(controllers): add create, update, delete and activate handlers

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -19,3 +19,44 @@ export async function getStudent(req: Request, res: Response, next: NextFunction
     next(err);
   }
 }
+
+export async function createStudent(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    const student = await studentService.create({ firstName, lastName, email, password });
+    res.status(201).json(student);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function updateStudent(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const id = Number(req.params.id);
+    const { firstName, lastName, email, password } = req.body;
+    const student = await studentService.update(id, { firstName, lastName, email, password });
+    res.json(student);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function deleteStudent(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const id = Number(req.params.id);
+    await studentService.deactivate(id);
+    res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function activateStudent(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const id = Number(req.params.id);
+    const student = await studentService.activate(id);
+    res.json(student);
+  } catch (err) {
+    next(err);
+  }
+}
