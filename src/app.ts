0de1af635bf6c5adcad47d412dcf328fac4f4ecf
FRANÇOISE Anthonia <hei.anthonia@gmail.com>
2026-08-24 23:55:38 +0300

    feat(exams): mount exam and question routes

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes";
 import { notFoundMiddleware } from "./middlewares/notFoundMiddleware";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
+import examRoutes from "./routes/examRoutes";
+import questionRoutes from "./routes/questionRoutes";
 
 const app = express();
 
@@ -10,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/exams", examRoutes);
+app.use("/api", questionRoutes);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
