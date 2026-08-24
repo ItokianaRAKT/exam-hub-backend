148e9873b482516bba046e57cd0e67bb96c06295
Itokiana <itokianarakt@gmail.com>
2026-08-24 09:31:24 +0300

    feat(controllers): add course list and get handlers

--- src/controllers/courseController.ts
+++ src/controllers/courseController.ts
@@ -1,1 +1,21 @@
-// TO DO
+import { Request, Response, NextFunction } from "express";
+import * as courseService from "../services/courseService";
+
+export async function listCourses(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const courses = await courseService.getAll();
+    res.json(courses);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function getCourse(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const id = Number(req.params.id);
+    const course = await courseService.getById(id);
+    res.json(course);
+  } catch (err) {
+    next(err);
+  }
+}
