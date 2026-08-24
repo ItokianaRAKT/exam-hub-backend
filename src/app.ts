e37f9320a746d65a73b660d5d3b13b3a332d82f5
Itokiana <itokianarakt@gmail.com>
2026-08-24 09:15:04 +0300

    feat(app): mount student routes at /api/students

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
+import studentRoutes from "./routes/studentRoutes";
 import { notFoundMiddleware } from "./middlewares/notFoundMiddleware";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/students", studentRoutes);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
