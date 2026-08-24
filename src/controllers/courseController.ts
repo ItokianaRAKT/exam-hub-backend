f3939ad6d8f87030b62a00856481e87fca406573
Itokiana <itokianarakt@gmail.com>
2026-08-24 09:32:09 +0300

    feat(controllers): add course create, update and delete handlers

--- src/controllers/courseController.ts
+++ src/controllers/courseController.ts
@@ -19,3 +19,34 @@ export async function getCourse(req: Request, res: Response, next: NextFunction)
     next(err);
   }
 }
+
+export async function createCourse(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const { code, name, description } = req.body;
+    const course = await courseService.create({ code, name, description });
+    res.status(201).json(course);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function updateCourse(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const id = Number(req.params.id);
+    const { code, name, description } = req.body;
+    const course = await courseService.update(id, { code, name, description });
+    res.json(course);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function deleteCourse(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const id = Number(req.params.id);
+    await courseService.remove(id);
+    res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+}
