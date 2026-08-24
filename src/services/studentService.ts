4668f43cecc96f5feaece5ab4050e2f3e8e77d46
Itokiana <itokianarakt@gmail.com>
2026-08-24 09:10:54 +0300

    feat(services): add getAll and getById student operations

--- src/services/studentService.ts
+++ src/services/studentService.ts
@@ -1,1 +1,15 @@
-// TO DO
+import * as userRepository from "../repositories/userRepository";
+import { User } from "../models/userModel";
+import { createApiError } from "../types/commonTypes";
+
+export async function getAll(role?: string): Promise<User[]> {
+  return userRepository.findAll(role);
+}
+
+export async function getById(id: number): Promise<User> {
+  const user = await userRepository.findById(id);
+  if (!user) {
+    throw createApiError("Étudiant introuvable", 404);
+  }
+  return user;
+}
