6a4674793d4356e853e78e2fb733c8fe4e7b03d2
Itokiana <itokianarakt@gmail.com>
2026-08-24 09:30:00 +0300

    feat(services): add getAll and getById course operations

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,1 +1,15 @@
-// TO DO
+import * as courseRepository from "../repositories/courseRepository";
+import { Course } from "../models/courseModel";
+import { createApiError } from "../types/commonTypes";
+
+export async function getAll(): Promise<Course[]> {
+  return courseRepository.findAll();
+}
+
+export async function getById(id: number): Promise<Course> {
+  const course = await courseRepository.findById(id);
+  if (!course) {
+    throw createApiError("Cours introuvable", 404);
+  }
+  return course;
+}
