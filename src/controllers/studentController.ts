c8cfcac461fc8c8302bee77be4a6e95c71b3edd8
Itokiana <itokianarakt@gmail.com>
2026-08-24 09:13:13 +0300

    feat(controllers): add list and get student handlers

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -1,1 +1,21 @@
-// TO DO
+import { Request, Response, NextFunction } from "express";
+import * as studentService from "../services/studentService";
+
+export async function listStudents(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const students = await studentService.getAll();
+    res.json(students);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function getStudent(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const id = Number(req.params.id);
+    const student = await studentService.getById(id);
+    res.json(student);
+  } catch (err) {
+    next(err);
+  }
+}
