0d36c09671ea2a682581127d59828324823acdde
Itokiana <itokianarakt@gmail.com>
2026-08-28 20:31:25 +0300

    feat(db): add migration 008 unique constraint and seed initial users
    - Add migration 008: unique constraint on attempts(exam_id, student_id)
    - Fill seed file with admin (admin@example.com) and student (student@example.com)

--- database/seeds/001_initial_admin.sql
+++ database/seeds/001_initial_admin.sql
@@ -1,1 +1,5 @@
--- TO DO
+INSERT INTO users (id, role, first_name, last_name, email, password_hash)
+VALUES
+  ('ADM001', 'admin', 'Admin', 'User', 'admin@example.com', '$2b$10$4Z4VCRWjWj1890sTYbMPGex2AqkkSXvhg9bE3x3OEYwnfw2zuOEA.'),
+  ('STU001', 'student', 'Jean', 'Dupont', 'student@example.com', '$2b$10$SMs851kG2EG2i/wc1qo67..QsV2845px4Qu3RhxyJHYhpLRqJ6hO2')
+ON CONFLICT (id) DO NOTHING;
